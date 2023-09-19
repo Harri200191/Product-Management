2dcf6ba63b7a72e400e59a76a808a704a51a4a4c
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-19 22:11:09 +0500

    Added controller and route for user updatation part

--- backend/routes/user_route.js
+++ backend/routes/user_route.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const user_model = require("../models/user_model");
-const {RegisterUser, LogInUser, LogOut, FetchData, LoginStatus} = require("../controllers/user_controller");
+const {RegisterUser, UpdateUser, LogInUser, LogOut, FetchData, LoginStatus} = require("../controllers/user_controller");
 const protect = require("../middleware/AuthMiddleware");
 
 const router = express.Router();
 
+// Protect middle ware is used only when we want to start the function IF the user is logged in
 router.post("/Register", RegisterUser);
 router.post("/Login", LogInUser);
 router.get("/Logout", LogOut);
 router.get("/FetchData", protect, FetchData);
 router.get("/LoggedIn", LoginStatus);
+router.patch("/UpdateUser", protect, UpdateUser);
 
 module.exports = router;
