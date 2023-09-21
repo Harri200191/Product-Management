2099c45f6b1a8cd2dddd89572319ea5f0b6e871c
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-21 16:42:53 +0500

    Added functionality to upload images of products in utils folder

---
+++ backend/utils/fileUpload.js
@@ -0,0 +1,17 @@
+const multer = require("multer");
+
+// Define file storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
+
+
