b519d15aa620f3495c6fff5e5ee1c9feefb95582
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-21 16:31:08 +0500

    Added create product controller

--- backend/controllers/product_controller.js
+++ backend/controllers/product_controller.js
@@ -8,7 +8,25 @@ const product_model = require("../models/productModel");
 
 // -------------------------------------------------------------------------------------
 const CreateProduct = asyncHandler(async (req, resp) =>{
+    const {name, sku, category, quantity, price, description} = req.body;
 
+    // Validate our request to check if params exist
+    if(!name || !sku || !category || !quantity || !price || !description){
+        resp.status(400);
+        throw new Error("Please fill in all fields");
+    };
+
+    const product = await product_model.create({
+        user: req.user.id,
+        name,
+        sku,
+        category,
+        quantity,
+        price,
+        description
+    });
+
+    resp.status(201).json(product);
 })
 // -------------------------------------------------------------------------------------
 
