c0d267736ee213c2d6ac80d99f18700658bd637c
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-23 00:09:32 +0500

    Update

---
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./Sidebar.scss";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { RiProductHuntLine } from "react-icons/ri";
+import SidebarItem from "./SidebarItem";
+import { useNavigate } from "react-router-dom";
+
+const Sidebar = ({ children }) => {
+
+  return (
+    <div className="layout">
+      <div className="sidebar" style={{ width: isOpen ? "230px" : "60px" }}>
+        <div className="top_section">
+          <div className="logo" style={{ display: isOpen ? "block" : "none" }}>
+            <RiProductHuntLine
+              size={35}
+              style={{ cursor: "pointer" }}
+            />
+          </div>
+
+          <div
+            className="bars"
+            style={{ marginLeft: isOpen ? "100px" : "0px" }}
+          >
+            <HiMenuAlt3  />
+          </div>
+        </div>
+        {menu.map((item, index) => {
+          return <SidebarItem key={index} item={item} isOpen={isOpen} />;
+        })}
+      </div>
+
+      <main
+        style={{
+          paddingLeft: isOpen ? "230px" : "60px",
+          transition: "all .5s",
+        }}
+      >
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import ProductList from "../../components/product/productList/ProductList";
+import ProductSummary from "../../components/product/productSummary/ProductSummary";
+import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
+import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
+import { getProducts } from "../../redux/features/product/productSlice";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <ProductSummary products={products} />
+      <ProductList products={products} isLoading={isLoading} />
+    </div>
+  );
+};
+
+export default Dashboard;
