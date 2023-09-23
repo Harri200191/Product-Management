b4038ac121fb61704157291a71909aedec172ef7
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-23 16:55:58 +0500

    Added a authslice for redux

--- frontend/src/redux/features/auth/authslice.js
+++ frontend/src/redux/features/auth/authslice.js
@@ -16,8 +16,29 @@ const initialState = {
 const authslice = createSlice({
   name: "auth",
   initialState,
-  reducers: {}
-});
+  reducers: {
+    SET_LOGIN(state, action) {
+      state.isLoggedIn = action.payload;
+    },
+    SET_NAME(state, action) {
+      localStorage.setItem("name", JSON.stringify(action.payload));
+      state.name = action.payload;
+    },
+    SET_USER(state, action) {
+      const profile = action.payload;
+      state.user.name = profile.name;
+      state.user.email = profile.email;
+      state.user.phone = profile.phone;
+      state.user.bio = profile.bio;
+      state.user.photo = profile.photo;
+    },
+  },
+});;
 
-export const {} = authslice.actions;
+export const { SET_LOGIN, SET_NAME, SET_USER } = authslice.actions;
+
+export const selectIsLoggedIn = (state) => state.auth.isLoggedIn;
+export const selectName = (state) => state.auth.name;
+export const selectUser = (state) => state.auth.user;
+
 export default authslice.reducer;
