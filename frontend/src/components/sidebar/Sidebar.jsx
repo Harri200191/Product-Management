fcd96111f2ee749460ff838b238ba02ee28a7012
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-23 10:31:08 +0500

    Added sidebar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
 import "./Sidebar.scss";
 import { HiMenuAlt3 } from "react-icons/hi";
-import { RiProductHuntLine } from "react-icons/ri";
+import {BiSolidDish} from "react-icons/bi";
 import SidebarItem from "./SidebarItem";
 import { useNavigate } from "react-router-dom";
+import menu from "../../data/sidebar";
 
 const Sidebar = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(true);
+  const toggle = () => setIsOpen(!isOpen);
+
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
 
   return (
     <div className="layout">
-      <div className="sidebar" style={{ width: isOpen ? "230px" : "60px" }}>
+      <div className="sidebar" style={{ width: isOpen ? "230px" : "60px" }} >
         <div className="top_section">
           <div className="logo" style={{ display: isOpen ? "block" : "none" }}>
-            <RiProductHuntLine
+            <BiSolidDish
               size={35}
               style={{ cursor: "pointer" }}
+              onClick={goHome}
             />
           </div>
 
-          <div
-            className="bars"
-            style={{ marginLeft: isOpen ? "100px" : "0px" }}
-          >
-            <HiMenuAlt3  />
+          <div className="bars" style={{ marginLeft: isOpen ? "130px" : "0px" }}>
+            <HiMenuAlt3
+              size={33}
+              style={{ cursor: "pointer" }}
+              onClick={toggle}
+            />
           </div>
         </div>
         {menu.map((item, index) => {
@@ -32,7 +43,6 @@ const Sidebar = ({ children }) => {
 
       <main
         style={{
-          paddingLeft: isOpen ? "230px" : "60px",
           transition: "all .5s",
         }}
       >
--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,16 +1,9 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import ProductList from "../../components/product/productList/ProductList";
-import ProductSummary from "../../components/product/productSummary/ProductSummary";
-import useRedirectLoggedOutUser from "../../customHook/useRedirectLoggedOutUser";
-import { selectIsLoggedIn } from "../../redux/features/auth/authSlice";
-import { getProducts } from "../../redux/features/product/productSlice";
 
 const Dashboard = () => {
   return (
     <div>
-      <ProductSummary products={products} />
-      <ProductList products={products} isLoading={isLoading} />
+      <h2>Dashboard</h2>
     </div>
   );
 };
