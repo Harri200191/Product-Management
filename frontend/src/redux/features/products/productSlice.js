921506ad952fed8ece27d559ef476e183a57ccd1
haris rehman <harisrehmanchugtai@gmail.com>
2023-09-24 17:03:50 +0500

    Added products service and slice

---
+++ frontend/src/redux/features/products/productSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import productService from "../../../services/productService";
+
+const initialState = {
+  product: null,
+  products: [],
+  isError: false,
+  isSuccess: false,
+  isLoading: false,
+  message: "",
+  totalStoreValue: 0,
+  outOfStock: 0,
+  category: [],
+};
+
+// Create New Product
+export const createProduct = createAsyncThunk(
+  "products/create",
+  async (formData, thunkAPI) => {
+    try {
+      return await productService.createProduct(formData);
+    } 
+    catch (error) {
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+        
+      console.log(message);
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    CALC_STORE_VALUE(state, action) {},
+  },
+
+  extraReducers: (builder) => {},
+});
+
+export const { CALC_STORE_VALUE } = productSlice.actions;
+export default productSlice.reducer;
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authreducer from "../redux/features/auth/authslice";
+import productReducer from "./features/products/productSlice";
 
 export const store = configureStore({
     reducer: {
-        auth: authreducer
+        auth: authreducer,
+        product: productReducer
     }
 });
