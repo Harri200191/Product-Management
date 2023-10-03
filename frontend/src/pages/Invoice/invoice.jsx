96d45479c82f2bcd8d9e143a06121189fd02c17e
haris rehman <harisrehmanchugtai@gmail.com>
2023-10-03 20:56:59 +0500

    Update

--- frontend/src/pages/Invoice/invoice.jsx
+++ frontend/src/pages/Invoice/invoice.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { PDFDownloadLink } from "@react-pdf/renderer";
 import InvoiceForm from "../../components/product/productForm/InvoiceForm";
 import { Document, Page, Text, View, StyleSheet } from "@react-pdf/renderer";
 import Card from "../../components/card/Card";
+import { SET_NAME, SET_USER } from "../../redux/features/auth/authslice";
+import { getUser } from "../../services/authservice";
 
 import "./Invoice.scss"
 
 const Invoice = () => {
+  const dispatch = useDispatch();
+
+  const [profile, setProfile] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => { 
+    setIsLoading(true);
+    async function getUserData() {
+      const data = await getUser(); 
+
+      setProfile(data);
+      setIsLoading(false);
+      await dispatch(SET_USER(data));
+      await dispatch(SET_NAME(data.name));
+    }
+    getUserData();
+  }, [dispatch]);
+
   const [products, setProducts] = useState([]);
   const [pdfData, setPdfData] = useState(null);
 
   const addProduct = () => {
-    const newProduct = { id: Date.now(), name: "", price: 0 };
+    const newProduct = { id: Date.now(), name: "", price: null, quantity: null };
     setProducts([...products, newProduct]);
   };
 
@@ -28,38 +49,105 @@ const Invoice = () => {
   };
 
   const generatePdf = () => {
+    const currentDate = new Date().toLocaleDateString();
+    const companyName = profile?.name;
+    const companyContact = "+"+ profile?.phone;
+  
     const pdfContent = (
       <Document>
         <Page size="A4">
-          <View style={styles.container}>
-            <Text style={styles.title}>Product Summary</Text>
+          {/* Header */}
+          <View style={styles.header}>
+            <Text style={styles.companyName}>{companyName}</Text> 
+            <Text>{companyContact}</Text>
+          </View>
+  
+          {/* Invoice Title */}
+          <Text style={styles.title}>Invoice</Text>
+  
+          {/* Invoice Items */}
+          <View style={styles.itemsContainer}>
+            <View style={styles.itemHeader}>
+              <Text>Product Name</Text>
+              <Text>Quantity</Text>
+              <Text>Price</Text>
+            </View>
             {products.map((product) => (
-              <View key={product.id} style={styles.product}>
+              <View key={product.id} style={styles.item}>
                 <Text>{product.name}</Text>
-                <Text>${product.price}</Text>
+                <Text>{product.quantity}</Text>  
+                <Text>Rs{product.price}</Text>
               </View>
             ))}
+          </View>
+  
+          {/* Total */}
+          <View style={styles.totalContainer}>
+            <Text>Total:</Text>
+            <Text>Rs{calculateTotal()}</Text>
+          </View>
+  
+          {/* Footer */}
+          <View style={styles.footer}>
+            <Text>Date: {currentDate}</Text> 
           </View>
         </Page>
       </Document>
     );
+  
     setPdfData(pdfContent);
   };
-
+  
+  const calculateTotal = () => {
+    const total = products.reduce((acc, product) => acc + product.price, 0);
+    return total.toFixed(2);
+  };
+  
   const styles = StyleSheet.create({
-    container: {
-      padding: 20,
+    header: {
+      padding: 10,
+      backgroundColor: "#0074D9",
+      color: "#fff",
+      marginBottom: 10,
+      textAlign: "center",
+    },
+    companyName: {
+      fontSize: 28,
+      fontWeight: "bold",
     },
     title: {
       fontSize: 24,
       marginBottom: 10,
+      textAlign: "center",
     },
-    product: {
+    itemsContainer: {
+      padding: 10,
+      marginBottom: 10,
+    },
+    itemHeader: {
       flexDirection: "row",
       justifyContent: "space-between",
-      marginBottom: 5,
+      backgroundColor: "#f2f2f2",
+      padding: 10,
+    },
+    item: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      padding: 10,
+    },
+    totalContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+      borderTop: 1,
+      padding: 10,
+    },
+    footer: {
+      padding: 10,
+      marginTop: 10,
+      textAlign: "right",
     },
   });
+  
 
   return (
     <div className="add-product">
@@ -77,7 +165,7 @@ const Invoice = () => {
         <br/>
         <button className= "btn2" onClick={generatePdf}>Generate PDF</button>
         {pdfData && (
-          <PDFDownloadLink document={pdfData} fileName="product_summary.pdf">
+          <PDFDownloadLink document={pdfData} fileName="Invoice.pdf">
             {({ blob, url, loading, error }) =>
               loading ? "Loading..." : "Download PDF"
             }
