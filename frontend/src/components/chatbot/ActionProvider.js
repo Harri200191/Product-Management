736e4af80b33d58c0d98d483d5db7f115790dea0
haris rehman <harisrehmanchugtai@gmail.com>
2023-10-04 22:16:29 +0500

    addedd chatbot config files

---
+++ frontend/src/components/chatbot/ActionProvider.js
@@ -0,0 +1,18 @@
+class ActionProvider {
+    constructor(
+     createChatBotMessage,
+     setStateFunc,
+     createClientMessage,
+     stateRef,
+     createCustomMessage,
+     ...rest
+   ) {
+     this.createChatBotMessage = createChatBotMessage;
+     this.setState = setStateFunc;
+     this.createClientMessage = createClientMessage;
+     this.stateRef = stateRef;
+     this.createCustomMessage = createCustomMessage;
+   }
+ }
+ 
+ export default ActionProvider;
--- frontend/src/components/chatbot/Chatbot.jsx
+++ frontend/src/components/chatbot/Chatbot.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Chatbot from "react-chatbot-kit";
+import MessageParser from './MessageParser';
+import ActionProvider from './ActionProvider';
+import config from './config';
 
 const Chatbot = () => {
   return (
     <div>
+      <Chatbot
+        config ={config}
+        actionProvider = {ActionProvider}
+        messageParser = {MessageParser}
+      />
+
       <h1>Chatbot For You</h1>
       <p>Just to test</p>
     </div>
